4ccf857d94c26bd0c4f17223bc7e04b9f493d188
govind4033 <govindpatel4033@gmail.com>
2026-04-28 14:41:27 +0530

    updated again url

--- todolist/backend/index.js
+++ todolist/backend/index.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "*",
-  origin: "https://friendly-mousse-c53720.netlify.app/",
+    origin: [
+    "http://localhost:5173",
+    "https://friendly-mousse-c53720.netlify.app"
+    ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 }));
 
